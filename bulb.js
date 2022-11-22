55622adf43200ea79f642bf569a2eef904700919
Małgorzata Zimnicka <114174585+MalgorzataZimnicka@users.noreply.github.com>
2022-11-22 23:50:23 +0100

    feat: add working toggle and button only in css

--- bulb.js
+++ bulb.js
@@ -1,29 +1,26 @@
 let btn = document.querySelector(".switch");
 let dot = document.querySelector(".dot");
+console.log(dot);
 
 let bulb = document.querySelector(".bulb-light");
 
-btn.addEventListener('click', active => {;
-    if (dot.classList.contains('activedot')){
-        dot.classList.remove('activedot');
-        dot.classList.add('passivedot');
-    }
-    else{
-        dot.classList.add('activedot')
-        dot.classList.remove('passivedot')
-    };
-    if (bulb.classList.contains('activelight')){
-        bulb.classList.remove('activelight');
-        bulb.classList.add('passivelight');
-    }
-    else{
-        bulb.classList.add('activelight')
-        bulb.classList.remove('passivelight')
-    }
-    
-    
-    //nieudana próba z toggle
-    //bulb.classList.toggle('activelight');
-    //bulb.classList.toggle('passivelight');
-})
+btn.addEventListener("click", (active) => {
+	//alternative with if
 
+	// let activecheck = ".activedot, .activelight";
+
+	// if (dot.matches(activecheck)) {
+	// 	dot.classList.remove("activedot");
+	// 	bulb.classList.remove("activelight");
+	// 	dot.classList.add("passivedot");
+	// 	bulb.classList.add("passivelight");
+	// } else {
+	// 	dot.classList.add("activedot");
+	// 	bulb.classList.add("activelight");
+	// 	dot.classList.remove("passivedot");
+	// 	bulb.classList.remove("passivelight");
+	// }
+
+	bulb.classList.toggle("activelight2");
+	dot.classList.toggle("activedot2");
+});
